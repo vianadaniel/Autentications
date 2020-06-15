fdd341399d219998e8fd0df869954bbed999197b
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2020-06-15 15:32:07 -0300

    1 level working

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.post("/resgister", function(req, res){
+app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
         password: req.body.password
 
     });
 
-    newUseer.save(function(err){
+    newUser.save(function(err){
         if (err) {
             console.log(err);
             
@@ -44,6 +44,27 @@ app.post("/resgister", function(req, res){
             res.render("secrets");
         }
     });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+            
+        } else {
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render("secrets")
+                }
+            }
+        }
+    })
+
+
+
 });
 
 app.listen(3000, function(){

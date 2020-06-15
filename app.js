1423b7e9a228aa28c3c2aa0b636c1e4d6cf68131
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2020-06-15 14:51:29 -0300

    registration

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -19,3 +29,24 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/resgister", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+
+    });
+
+    newUseer.save(function(err){
+        if (err) {
+            console.log(err);
+            
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Servidor porta 3000");
+    
+});
